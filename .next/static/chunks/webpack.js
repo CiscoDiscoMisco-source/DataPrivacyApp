9af915bed9262e08226fe1f14ad9d48474ecab83
vicente <vicente.carrasco@leprcon.com>
2025-03-18 16:37:01 -0300

    Update webpack cache files and binary pack files for client and server development; refresh layout styles and ensure consistency in CSS handling across the application.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "74250d6b0510edc9"; }
+/******/ 		__webpack_require__.h = function() { return "9a7ac1cedda150f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
